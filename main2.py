3a54263ba87380d561487fe9b566d32cf8d63638
isaac <isaacyong@naver.com>
2025-11-07 13:27:19 +0900

    main2

--- main2.py
+++ main2.py
@@ -14,11 +14,19 @@ class ParamSet:
     def to_numpy(self) -> np.ndarray:
         return np.array([self.thickness, self.roughness, self.sld], dtype=np.float64)
 
-def tth2q_wavelen[T: (float, np.ndarray)](tth: T, wavelen: float) -> T:
+    def __format__(self, format_spec: str) -> str:
+        """클래스 전체에 포매팅 명령어 적용 (.3f, .2f 등)"""
+
+        formatted = [f"{key}={val:{format_spec}}" for key, val in self.__dataclass_fields__.items()]
+        return f"{self.__class__.__name__}({formatted})"
+
+
+def tth2q_wavelen[T: (float, np.ndarray)](tth: T, wavelen: float = 1.54) -> T:
     """
     Convert 2θ (in degrees) and wavelength (in Å) to scattering vector q (in 1/Å).
     tth: degree
     wavelen: Å
+    default Kalpha = 1.54
 
     -> q: 1/Å
     """
@@ -42,9 +50,9 @@ def build_structure(params: list[ParamSet]) -> Structure:
 
 def compute_reflectivity(structure: Structure, q: np.ndarray) -> np.ndarray:
     model = ReflectModel(structure)
-    return model(q)
+    return model(10 * q) # nm -> Å
 
-def xrr_simulate(qs, params: list[ParamSet], has_noise=False) -> np.ndarray:
+def xrr_simulate(qs, params: list[ParamSet], /, has_noise=False) -> np.ndarray:
     structure = build_structure(params)
     refl = compute_reflectivity(structure, qs)
 
@@ -60,12 +68,23 @@ class ParamQuantizer:
     각 파라미터마다 개별 bins를 지정하여 양자화한다.
     예: thickness_bins, roughness_bins, sld_bins
     """
-    def __init__(self, thickness_bins: np.ndarray,
-                 roughness_bins: np.ndarray,
-                 sld_bins: np.ndarray):
+    def __init__(
+        self,
+        thickness_bins: np.ndarray,
+        roughness_bins: np.ndarray,
+        sld_bins: np.ndarray
+    ):
+
         self.thickness_bins = thickness_bins
         self.roughness_bins = roughness_bins
         self.sld_bins = sld_bins
+
+        if self.thickness_bins is None:
+            self.thickness_bins = np.linspace(0, 2000, 51)
+        if self.roughness_bins is None:
+            self.roughness_bins = np.linspace(0, 10, 21)
+        if self.sld_bins is None:
+            self.sld_bins = np.linspace(1.0, 6.0, 26)
 
         # 라벨은 자연스럽게 구간 번호로 지정
         self.thickness_labels = np.arange(len(thickness_bins) - 1)
@@ -100,7 +119,7 @@ class XRRPipeline:
     def __init__(self, quantizer: ParamQuantizer):
         self.quantizer = quantizer
 
-    def simulate_and_label(self, q: np.ndarray, params: list[ParamSet], has_noise=False):
+    def simulate_and_label(self, qs: np.ndarray, params: list[ParamSet], /, has_noise=False) -> dict:
         """
         params: multi-layer (list of ParamSet)
         return: dict with keys:
@@ -109,14 +128,73 @@ class XRRPipeline:
             - "params": list[ParamSet]
         """
 
-        refl = xrr_simulate(q, params, has_noise=has_noise)
+        refl = xrr_simulate(qs, params, has_noise=has_noise)
 
         # multi-layer인 경우, 전체 ParamSet을 독립적으로 quantize 후 concatenation
         all_labels = [self.quantizer.quantize(p) for p in params]
         lab = np.concatenate(all_labels, axis=0)
 
         return {
-            "params": params,           # 원본 실수 파라미터
+            "param": params,           # 원본 실수 파라미터
             "reflectivity": refl,       # XRR curve
             "label": lab,               # 양자화 라벨 (길이 = layer_count * 3)
         }
+
+
+def main() -> None:
+    import itertools
+    import random
+
+
+    # Material Configurations
+    thick_min, thick_max = 5.0, 2000.0    # Å
+    sld_min, sld_max = 0.0, 140.0    # x1e-6 Å^-2
+    rough_min, rough_max = 0.0, 10.0  # Å, 주로 0-10Å 세밀
+
+    params: list[ParamSet] = []
+    for _ in range(10):
+        thickness = random.uniform(thick_min, thick_max + 1e-9)
+        roughness = random.uniform(rough_min, rough_max + 1e-9)
+        sld = random.uniform(sld_min, sld_max + 1e-9)
+        params.append(ParamSet(thickness, roughness, sld))
+
+    # Measurement Configurations
+    wavelen: float = 1.54  # (Å)
+    tth_min: float = 1.0   # degree
+    tth_max: float = 6.0
+    q_min: float = tth2q_wavelen(tth_min, wavelen)  # (1/Å)
+    q_max: float = tth2q_wavelen(tth_max, wavelen)
+    q_n: int = 100
+    qs: np.ndarray = np.linspace(q_min, q_max, q_n)
+
+    n_thick_coarse = 50    # coarse bins (log)
+    n_sld_coarse = 40   # linear
+    n_rough_coarse = 20   # linear
+
+    # Quantization Configurations
+    # bins
+    thick_bins = np.logspace(np.log10(thick_min), np.log10(thick_max), n_thick_coarse+1)
+    sld_bins = np.linspace(sld_min, sld_max, n_sld_coarse+1)
+    rough_bins = np.linspace(rough_min, rough_max, n_rough_coarse+1)
+
+    quantizer = ParamQuantizer(
+        thickness_bins=thick_bins,
+        roughness_bins=rough_bins,
+        sld_bins=sld_bins,
+    )
+
+    print("Range of q:", q_min, q_max)
+    # Data Simuation
+    xrr_pipeline = XRRPipeline(quantizer)
+    dataset: dict = xrr_pipeline.simulate_and_label(qs, params, has_noise=True)
+    for param, label in zip(
+        dataset["param"],
+        itertools.batched(dataset["label"], 3),
+        strict=True
+        ):
+
+        print(f"{param:.3f}\n{label}")
+
+
+if __name__ == "__main__":
+    main()
